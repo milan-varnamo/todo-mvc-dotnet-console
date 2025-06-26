701527f105428149a00432642af61a0128149e44
milan-varnamo <milan.mlinar.sv@gmail.com>
2025-06-26 18:24:44 +0200

    Add Todo model

---
+++ todo-mvc-dotnet-console/Models/Todo.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TodoList.App.Models
+{
+	public class Todo
+	{
+		public Guid Id { get; } = Guid.NewGuid();
+		public string _description;
+
+		public string Description
+		{
+			get => _description;
+			set
+			{
+
+				if (string.IsNullOrWhiteSpace(value))
+				{
+					throw new ArgumentNullException("The description can't be empty.");
+				}
+				else
+				{
+					_description = value;
+				}
+			}
+		}
+
+		public Todo(string description)
+		{
+			Description = description;
+		}
+	}
+}
