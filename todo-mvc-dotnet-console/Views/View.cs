c94f9b0ae7fe871c2f381d74862b6816f0da1857
milan-varnamo <milan.mlinar.sv@gmail.com>
2025-06-30 22:04:27 +0200

    Add View class with ActionTaken enum, RenderLogo(), and RenderMainMenu() methods

---
+++ todo-mvc-dotnet-console/Views/View.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TodoList.App.Views
+{
+	internal class View
+	{
+		public enum ActionTaken
+		{
+			Quit,
+			Add,
+			Edit,
+			Delete,
+			None
+		}
+
+		public void RenderLogo()
+		{
+			Console.Clear();
+			Console.ForegroundColor = ConsoleColor.Green;
+			Console.WriteLine("X======================X");
+			Console.WriteLine("X                      X");
+			Console.WriteLine("X     My Todo List     X");
+			Console.WriteLine("X                      X");
+			Console.WriteLine("X======================X");
+			Console.ResetColor();
+		}
+
+		public void RenderMainMenu() 
+		{
+			RenderLogo();
+			Console.WriteLine("1. Show my Todos");
+			Console.WriteLine("2. Add a Todo");
+			Console.WriteLine("3. Edit a Todo");
+			Console.WriteLine("4. Delete a todo");
+			Console.WriteLine("0. Quit the program");
+		}
+	}
+}
